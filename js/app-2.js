b0787dcdcf1c3335f3ffe6f736bb914d75e56902
Peter Čagala <petercagala@gmail.com>
2020-03-24 15:05:52 +0100

    [97-1] Progress indicator 2

--- js/app-2.js
+++ js/app-2.js
@@ -1,7 +1,25 @@
 'use strict';
 
-const progessBar = document.querySelector('progress');
+const progressBar = document.querySelector('progress');
+// Dlzka celeho dokumenu - dlzka viditelnej casti stranky v 
+// mojom okne prehliadaca
+progressBar.max = calculateProgressBarMax();
 
-addEventListener('scroll', function scrollEvent(event) {
-    console.log(Date.now());
+/**
+ * window tam vobec nemusi byt, lebo je to globalny objekt. Ak nezadam window,
+ * tak sa chytam objektu window
+ */
+window.addEventListener('scroll', function scrollEvent(event) {
+
+   // Nastavime pocet pixelov o ktore som sa odscrolloval od vrchu stranky
+   progressBar.value = window.pageYOffset;
+//    console.log(window.pageYOffset);
 });
+
+window.addEventListener('resize', function resizeEvent() {
+    progressBar.max = calculateProgressBarMax();
+});
+
+function calculateProgressBarMax() {
+    return document.body.scrollHeight - window.innerHeight;
+}
