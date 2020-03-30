397b687b81dc3c3a622aa28b2210188842f44203
Peter Čagala <petercagala@gmail.com>
2020-03-30 15:50:49 +0200

    [112-1] DOM vykon 02, prvy velky performance test

--- lesson-107-palinove-gule/js/perforance.js
+++ lesson-107-palinove-gule/js/perforance.js
@@ -1,20 +1,48 @@
 'use strict'
 
+const cycles = 30000;
+const measurementsElement = document.querySelector(".measurements");
+const testName = "PERFORMANCE TEST 01";
+
+
+
+
+
+// START TEST
 let timeStart = performance.now();
-console.time("PERFORMANCE TEST 01");
+console.time(testName);
 
-for(let i = 0; i <50000; i++) {
-    document.querySelector("body").style.width = i + "px";
+for(let i = 0; i < cycles; i++) {
+    var unorderedList = document.querySelector(".super-test");
 
-    // Pridanie jedneho riadku kodu pomaly zdvojnasobi potrebny vypoctovy cas
-    let w = document.querySelector("body").style.width;
+    var listElement = document.createElement("li");
+    listElement.innerHTML = i;
+
+    unorderedList.appendChild(listElement);
 }
 
-let timeEnd = performance.now();
 
-// Ide to aj tak, ze "PERFORMANCE TEST 01" dam do console.time aj do console.timeEnd
-console.timeEnd("PERFORMANCE TEST 01");
 
-console.log(
-     (timeEnd - timeStart).toFixed(3) + "ms"
-);
+// Premaz vsetky li elementy
+cleanListelement(document.querySelector(".super-test"));
+
+
+// Vysledky testu 01
+getPerformanceResults("01", timeStart);
+
+
+
+// Apparently faster than ul.innerHTML = '';
+function cleanListelement(unorderedList) {
+    while(unorderedList.firstChild) {
+        unorderedList.removeChild(unorderedList.firstChild);
+    }
+}
+
+function getPerformanceResults(testNumber, timeStart) {
+    let timeEnd = performance.now();
+    // Ide to aj tak, ze "PERFORMANCE TEST 01" dam do console.time aj do console.timeEnd
+    console.timeEnd("PERFORMANCE TEST " + testNumber);
+
+    measurementsElement.innerHTML = `<small>${testNumber}</small>: ${(timeStart - timeEnd).toFixed(3)}`;
+}
